c863dbb773cedd4bfaeb8aa00643177ecae41ccd
Andreï Barannikov <andrebarannikov06@gmail.com>
2026-07-17 10:20:44 +0300

    Create truefalse.py

---
+++ truefalse.py
@@ -0,0 +1,8 @@
+#This program shows if the first number that you enter is bigger than the second number you enter.
+#If the first number is bigger than the second number, the program prints "True".
+#If the first number is equal or smaller than the second, the program prints "False".
+
+print("Please enter the two numbers ?>?")
+a = int(input())
+b = int(input())
+print(a>b)
